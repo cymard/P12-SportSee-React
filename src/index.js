204107ab9c17d63f2da8d0feef9eaae0440b35ba
cyrian simard <cyriansimard@gmail.com>
2023-03-24 06:29:37 +0100

    add header + fix router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Router from './router';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './layouts/Header/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Router />
+        <Header />
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
     </React.StrictMode>
 );
 
