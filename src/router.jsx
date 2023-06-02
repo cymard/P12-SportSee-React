7e1be7770bf3ed6734866c9d685638fa8d8c8156
cyrian simard <cyriansimard@gmail.com>
2023-06-02 14:40:15 +0200

    feat: redirect to mock/12  when no route matched

--- src/router.jsx
+++ src/router.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 
 function Router() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        const uriRegex = /\/home\/(?:mock\/)?\d+/;
+
+        if (!uriRegex.test(location.pathname)) {
+            navigate('/home/mock/12');
+        }
+    }, [location.pathname]);
+
     return (
         <Routes>
             <Route path="/home/:id" element={<Home isMock={false} />} />
